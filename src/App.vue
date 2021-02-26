b30b14e31884df59f77dd7b97bbbba37f2806031
hi-matbub <6matbub@gmail.com>
2021-02-26 15:22:18 -0800

    feat: static video

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <MediaPlayer />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import MediaPlayer from './components/MediaPlayer';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    MediaPlayer,
   },
 };
 </script>
