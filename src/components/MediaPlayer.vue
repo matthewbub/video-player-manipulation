91b5b78f5ad8d83b06c974aa3666a61f1e592b6a
hi-matbub <6matbub@gmail.com>
2021-02-26 18:35:47 -0800

    feat: adding .webm video format

--- src/components/MediaPlayer.vue
+++ src/components/MediaPlayer.vue
@@ -1,34 +1,31 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <video controls width="250">
+    <source src="../assets/big_buck_bunny.webm"
+      type="video/webm"
+    >
+    <source src="../assets/big_buck_bunny.mp4"
+      type="video/mp4"
+    >
+    Sorry, your browser doesn't support embedded videos.
+  </video>
 </template>
 
 <script>
 export default {
   name: 'MediaPlayer',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
+  // data() {
+  //   return {
+  //     msg: 'Welcome to Your Vue.js App',
+  //   };
+  // },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-} */
+  div {
+    height: 300px;
+    width: 300px;
+    background: grey;
+  }
 </style>
