8bbf062c6d81c2d63781d3b329934c5bdbc92e3b
hi-matbub <6matbub@gmail.com>
2021-02-26 18:14:55 -0800

    feat: init vue-material (#3)

--- src/components/MediaPlayer.vue
+++ src/components/MediaPlayer.vue
@@ -5,6 +5,13 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default-dark.css';
+
+Vue.use(VueMaterial);
+
 export default {
   name: 'MediaPlayer',
   data() {
