742e4ef8e43dba3a1f8fa53556a02b7414cf02cd
hi-matbub <6matbub@gmail.com>
2021-02-26 21:28:05 -0800

    feat: init button with toggle func

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <!-- <img src="./assets/logo.png"> -->
     <!-- <HelloWorld/> -->
     <MediaPlayer />
+    <Controls/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
 import MediaPlayer from './components/MediaPlayer';
+import Controls from './components/Controls';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     MediaPlayer,
+    Controls,
   },
 };
 </script>
--- src/components/MediaPlayer.vue
+++ src/components/MediaPlayer.vue
@@ -13,8 +13,9 @@
 <script>
 import Vue from 'vue';
 import VueMaterial from 'vue-material';
-import 'vue-material/dist/vue-material.min.css';
-import 'vue-material/dist/theme/default-dark.css';
+// Breaking test suites 🤔
+// import 'vue-material/dist/vue-material.min.css';
+// import 'vue-material/dist/theme/default-dark.css';
 
 Vue.use(VueMaterial);
 
